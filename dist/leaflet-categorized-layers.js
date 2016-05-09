126277195016cfa6f9b1c4a05f47bd7383979967
NielsHolt <niels@steenbuchholt.dk>
2016-05-09 16:11:42 +0200

    Bug fix

--- dist/leaflet-categorized-layers.js
+++ dist/leaflet-categorized-layers.js
@@ -10,7 +10,7 @@
 
 L.Control.CategorizedLayers = L.Control.Layers.extend({
   options: {
-		VERSION: "0.0.9",
+		VERSION: "0.1.0",
     collapsed: true,
     groupsCollapsed: true,
     collapseActiveGroups: false,
